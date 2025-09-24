d35bb4e0635ed7c280d86be8f585d03c058485dd
Arthur Hsu <arthur.hsu17@gmail.com>
2025-09-24 15:13:00 +0100

    Update insights.py
    change page_size from 5000 to 1000

--- spgci/insights.py
+++ spgci/insights.py
@@ -1101,7 +1101,7 @@ class Insights:
         strip_html: bool = False,
         filter_exp: Optional[str] = None,
         page: int = 1,
-        page_size: int = 5000,
+        page_size: int = 1000,
         paginate: bool = False,
         raw: bool = False,
     ) -> Union[DataFrame, Response]:
