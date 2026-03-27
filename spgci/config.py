cc8110c9d120fcaf22971eb127b5735bd02a4aed
achristie <awchristie86@gmail.com>
2026-03-27 11:38:00 -0400

    fix typo

--- spgci/config.py
+++ spgci/config.py
@@ -63,7 +63,7 @@ proxies: Dict[str, str] = {
 auth: Union[AuthBase, None] = None
 
 #: Version of the SPGCI Pkg
-version = "0.0.81"
+version = "0.0.82"
 
 #: time to sleep between api calls
 sleep_time = 0
